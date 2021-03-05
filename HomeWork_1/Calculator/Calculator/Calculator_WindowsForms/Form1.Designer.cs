3b06cb6f86933f2061557219e64c069a38808efd
Lisnn <953147597@qq.com>
2021-03-05 16:12:11 +0800

    重新整理了格式

---
+++ HomeWork_1/Calculator/Calculator/Calculator_WindowsForms/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Calculator_WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "计算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 123);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(331, 123);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBox1.Location = new System.Drawing.Point(204, 124);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 340);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ HomeWork_2/FactorFind/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int div = 2;//div作除数
+            int[] res = new int[100];//res存储结果
+            int i = 0;//i记录数组下标
+            if(IsPri(num1))
+            {
+                Console.WriteLine("该数字本身就是素数");
+            }
+            else
+            {
+                while (!IsPri(num1))
+                {
+                    num1 = num1 / div;
+                    res[i] = div;
+                    do
+                    {
+                        div++;
+                    } while (!IsPri(div));
+                    i++;
+                    if(i>98)
+                    {
+                        Console.WriteLine("数字过大，无法完全显示");
+                        break;
+                    }
+                }
+                res[i] = num1;
+            }
+            while (i >= 0)
+            {
+                Console.Write(" " + res[i]);
+                i--;
+            }
+            Console.ReadKey();
+        }
+
+        public static bool IsPri(int num2)
+        {
+            if (num2 <= 0)
+            {
+                Console.WriteLine("请输入正确的数值!");
+                return false;
+            }
+            else if (num2 == 1)
+                return false;
+            int i = num2 - 1;
+            while(i>1)
+            {
+                if (num2 % i == 0)
+                    return false;
+                i--;
+            }
+            return true;
+        }
+    }
+}
